cd1a2687e614fce5636bea867a9bbcca8f7b7855
bocai <woaichibocai_hxb@163.com>
2018-01-28 15:32:54 +0800

    demos

---
+++ pickles/make_db_pickle_recs.py
@@ -0,0 +1,9 @@
+import sys
+# 把上级目录加入系统路径
+sys.path.append('../')
+from initdata import bob, sue, tom
+import pickle
+for(key, record) in [('bob', bob),('tom', tom),('sue', sue)]:
+    recfile = open(key + '.pkl', 'wb')
+    pickle.dump(record, recfile)
+    recfile.close()
